8e740f1f2c1326b5964f8b7a47bce1c452f7899c
William Thompson <wmthmps1@gmail.com>
2018-01-03 00:35:18 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,41 @@
+def position_taken?(board,index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    return false
+   else board[index] == "X" or board[index] == "O"
+    return true
+  end
+end
+
+
+
+def valid_move?(board,index)
+  board[index].to_i-1
+  if position_taken?(board,index) == true
+    return false
+  elsif index > 9 || index < 0
+    return false
+  else
+    return true
+  end
+end
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+# code your input_to_index and move method here!
+def input_to_index(user_input)
+  user_input.to_i-1
+end
+
+def move(board,index,character="X")
+  board[index] = character
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+end
